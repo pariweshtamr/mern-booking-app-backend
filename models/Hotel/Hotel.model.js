36800ea30515ac69f6c396a6199be0b594b76937
Pariwesh <tamrpariwesh@gmail.com>
2022-05-13 18:11:17 +1000

    added update method for hotel

--- models/Hotel/Hotel.model.js
+++ models/Hotel/Hotel.model.js
@@ -1,10 +1,19 @@
 import Hotel from './Hotel.schema.js'
 
-// Add Hotel
-
+//Add Hotel
 export const addHotel = (newHotel) => {
   try {
     const hotel = Hotel(newHotel).save()
+    return hotel
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+//Update hotel
+export const updateHotel = (_id, updatedHotel) => {
+  try {
+    const hotel = Hotel.findByIdAndUpdate(_id, updatedHotel, { new: true })
     return hotel
   } catch (error) {
     console.log(error)
--- routers/hotels.js
+++ routers/hotels.js
@@ -1,14 +1,27 @@
 import express from 'express'
-import { addHotel } from '../models/Hotel/Hotel.model.js'
-import Hotel from '../models/Hotel/Hotel.schema.js'
+import { addHotel, updateHotel } from '../models/Hotel/Hotel.model.js'
 
 const hotelsRouter = express.Router()
 
-//CREATE
+//ADD
 hotelsRouter.post('/', async (req, res) => {
   try {
     const hotel = await addHotel(req.body)
     res.status(200).json(hotel)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+//UPDATE
+hotelsRouter.put('/:id', async (req, res) => {
+  try {
+    const updatedhotel = await updateHotel(
+      req.params.id,
+      { $set: req.body },
+      { new: true },
+    )
+    res.status(200).json(updatedhotel)
   } catch (error) {
     res.status(500).json(error)
   }
